e0b3a2c8f860c2c281b2f1a0d26bb9c1d6fb17e3
Marcellinus Elbert <51221428+marcellinuselbert@users.noreply.github.com>
2021-12-06 10:06:29 +0700

    implement fetch API (#3)

--- src/components/card.js
+++ src/components/card.js
@@ -1,23 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function FilmCard() {
+export default function FilmCard({ film }) {
   return (
     <div className="w-60 h-80 rounded-md shadow-lg transition duration-500 ease-in-out transform hover:translate-y-1 hover:scale-110">
       <Link to="/film" className="h-full w-full">
         <div className="w-full h-1/2">
           <img
             className="object-cover h-full w-full rounded-md"
-            src="https://upload.wikimedia.org/wikipedia/id/0/0d/Avengers_Endgame_poster.jpg"
+            src={film.imageUrl}
           ></img>
         </div>
         <div className="w-full h-1/2">
-          <div className="w-full h-3/4 text-center p-5">
-            <p>Avengers</p>
-          </div>
+          <div className="w-full h-3/4 text-center p-5">{film.title}</div>
           <div className="w-full h-1/4 flex justify-between px-5">
-            <p>Action</p>
-            <p>2019</p>
+            <p>{film.genre}</p>
+            <p>{film.released_year}</p>
           </div>
         </div>
       </Link>
