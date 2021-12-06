86d51ae1fa8a15983e7ab97a53bd7a24c087210f
marcellinuselbert11 <marcellinuselbert46@gmail.com>
2021-12-06 10:04:45 +0700

    implement fetch API

--- src/components/table.js
+++ src/components/table.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FilmCard from "./card";
 
 export default function Table() {
+  const [films, setFilms] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    fetch("https://pti-final-project-be.herokuapp.com", { method: "GET" })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setFilms(result.data);
+          console.log(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
   return (
     <div className="w-auto h-auto flex items-center justify-center  ">
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 m-5">
-        <FilmCard />
-        <FilmCard />
-        <FilmCard />
-        <FilmCard />
-        <FilmCard />
-        <FilmCard />
-        <FilmCard />
-        <FilmCard />
-        <FilmCard />
-        <FilmCard />
+        {films.map((film) => (
+          <FilmCard film={film} />
+        ))}
       </div>
     </div>
   );
