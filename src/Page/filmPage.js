6fd263cd7ac9c81c9d5a5eecfabd6c5ed0b7209a
al-ayubi2020 <al.ayubi2020@gmail.com>
2021-12-06 08:56:37 +0700

    animate and film detail

--- src/Page/filmPage.js
+++ src/Page/filmPage.js
@@ -1,5 +1,50 @@
-import React from "react";
+  import React from "react";
+import { Link } from "react-router-dom";
 
 export default function FilmPage() {
-  return <div className="w-screen h-screen border border-red-600"></div>;
+  return (
+    <div className="w-screen h-screen ">
+      <div className="w-full h-1/6 flex items-center justify-center lg:justify-start shadow-lg rounded-b-lg sticky">
+        <div className="flex items-center gap-10 justify-between lg:ml-10">
+          <Link to="/">Home</Link>
+          <Link to="/lastest" className="text-indigo-600">
+            Lastest
+          </Link>
+          <Link to="/popular">Most Like</Link>
+        </div>
+      </div>
+      <div className="w-auto h-5/6 flex">
+        <div className="w-1/2 h-full flex items-center justify-center">
+          <div className='w-auto h-auto'>
+            <div className='grid grid-cols-2 gap-5 text-xl'>
+              <div>Judul</div>
+              <div>Avengers</div>
+              <div>Genre</div>
+              <div>Super Hero</div>
+              <div>Tahun Rilis</div>
+              <div>2018</div>
+              <div className='col-span-2 w-full h-full'>
+                <button className='h-full w-full bg-indigo-600 text-white rounded-md p-2 transition duration-500 ease-in-out transform hover:translate-y-1 hover:scale-110'>
+                <Link to="/">Trailer</Link>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-1/2 h-full ">
+          <div className="h-full w-full relative">
+            <img
+              className="object-cover h-full w-full"
+              src="https://upload.wikimedia.org/wikipedia/id/0/0d/Avengers_Endgame_poster.jpg"
+            ></img>
+            <div className="w-full h-full bg-gray-400 absolute bottom-0 left-0 bg-opacity-30">
+              <p className="absolute bottom-0 left-0 text-white text-3xl m-5 w-auto">
+                Avengers
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/table.js
+++ src/components/table.js
@@ -4,7 +4,7 @@ import FilmCard from "./card";
 export default function Table() {
   return (
     <div className="w-auto h-auto flex items-center justify-center  ">
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 m-5">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 m-5">
         <FilmCard />
         <FilmCard />
         <FilmCard />
