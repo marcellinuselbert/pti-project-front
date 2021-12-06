c5d7590604d18bfcf00a6f0a6202cded7b686b1f
al-ayubi2020 <al.ayubi2020@gmail.com>
2021-12-06 09:30:09 +0700

    install axios

--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function FilmCard() {
+export default function FilmCard(props) {
   return (
     <div className="w-60 h-80 rounded-md shadow-lg transition duration-500 ease-in-out transform hover:translate-y-1 hover:scale-110">
       <Link to="/film" className="h-full w-full">
@@ -13,11 +13,11 @@ export default function FilmCard() {
         </div>
         <div className="w-full h-1/2">
           <div className="w-full h-3/4 text-center p-5">
-            <p>Avengers</p>
+            <p>{props.title}</p>
           </div>
           <div className="w-full h-1/4 flex justify-between px-5">
-            <p>Action</p>
-            <p>2019</p>
+            <p>{props.genre}</p>
+            <p>{props.year}</p>
           </div>
         </div>
       </Link>
