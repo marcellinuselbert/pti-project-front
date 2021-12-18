e28806812de01344b6ec7ee289b852e073703f4a
Marcellinus Elbert <51221428+marcellinuselbert@users.noreply.github.com>
2021-12-18 21:35:37 +0700

    implement sort by popularity (#9)

--- src/Page/Popular.js
+++ src/Page/Popular.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Section from "../components/section";
-import Table from "../components/table";
+import FilmCard from "../components/card";
 
 export default function Popular() {
+  const [films, setFilms] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    fetch("https://pti-final-project-be.herokuapp.com?sort=likes", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setFilms(result.data);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, [films]);
   return (
     <div className="w-auto h-auto">
       <div className="w-full h-20 flex items-center justify-center lg:justify-start shadow-lg rounded-b-lg sticky">
@@ -18,7 +37,13 @@ export default function Popular() {
       <div className="w-full h-28 flex items-center px-10 text-xl font-semibold">
         <p>Film berdasarkan Popularitas</p>
       </div>
-      <Table />
+      <div className="w-auto h-auto flex items-center justify-center  ">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 m-5">
+          {films.map((film) => (
+            <FilmCard film={film} />
+          ))}
+        </div>
+      </div>
       <Section />
     </div>
   );
